b59810a45f095a1b961ea19cc80f34ad0d920038
Steven Schmatz <stevenschmatz@gmail.com>
2014-10-30 15:14:12 -0400

    Revert d0eacf8..281311d
    This rolls back to commit d0eacf894f002b9d81a86585d6cad2188f933274.

--- BlueBus/FirstViewController.swift
+++ BlueBus/FirstViewController.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let cellIdentifier = "cellIdentifier"
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: false)
+        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.Default, animated: false)
 
         self.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Lobster Two", size: 34)!]
+        
+        // Register the UITableViewCell class with the tableView
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
 
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -29,5 +35,33 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // ===================================
+    // MARK: UITableViewDataSource methods
+    // ===================================
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: self.cellIdentifier)
+        
+        cell.textLabel.text = "Bursley-Baits"
+        cell.detailTextLabel?.text = "10 minutes"
+        
+        cell.textLabel.font = UIFont(name: "Lobster Two", size: 30)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return CGFloat(100)
+    }
+ 
+   
     
 }
